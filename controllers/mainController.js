96c80e07343bdbe25e633c268a5b464540d5609e
Florimond <florimond.husquinet@gmail.com>
2016-08-18 20:08:53 +0200

    menu navigation update

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,7 @@
 var mainController = function ($scope)
 {
 	$scope.view = "templates/menu.html";
-	$scope.startGame = function(){ $scope.view = "templates/startGame.html";}
+	$scope.startGameMenu = function(){ $scope.view = "templates/startGameMenu.html";}
+	$scope.connectToGameMenu = function(){ $scope.view = "templates/connectToGameMenu.html";}
+	$scope.startGame = function(){$scope.view = "templates/board.html";}
 };
