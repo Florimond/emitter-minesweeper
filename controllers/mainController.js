1917d3f41ac563aa85fa6c8673f434ee51053501
Florimond <florimond.husquinet@gmail.com>
2016-08-18 22:35:08 +0200

    game engine update

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -3,13 +3,21 @@ var mainController = function ($scope)
 	$scope.view = "templates/menu.html";
 	$scope.startGameMenu = function()
 	{
-		$scope.playerId = 0;
 		$scope.view = "templates/startGameMenu.html";
-	}
+	};
 	$scope.connectToGameMenu = function()
 	{ 
-		$scope.playerId = 1;
 		$scope.view = "templates/connectToGameMenu.html";
+	};
+	$scope.startGame = function()
+	{
+		$scope.thisPlayerId = 0;
+		$scope.view = "templates/board.html";
 	}
-	$scope.startGame = function(){$scope.view = "templates/board.html";}
+
+	$scope.connectToGame = function() 
+	{
+		$scope.thisPlayerId = 1;
+		$scope.view = "templates/board.html";
+	};
 };
