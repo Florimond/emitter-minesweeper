8bd911dcc8243870c96268999d215661491d3ebf
Florimond <florimond.husquinet@gmail.com>
2017-10-26 19:41:47 +0200

    Work in progress: lobby + guidGenerator from module to service

--- services/emitterService.js
+++ services/emitterService.js
@@ -3,6 +3,7 @@ var emitterService = function (emitterKey, baseChannel)
 	var emitter = null;	
 	return	{
 		connect: function(connectionHandler){
+			
 			emitter = window.emitter.connect({/*host: "10.0.0.20"*//*, secure: true*/ });
 			emitter.on("connect", connectionHandler);
 		},
@@ -25,6 +26,18 @@ var emitterService = function (emitterKey, baseChannel)
 				})
 			});
 		},
+		presence: function(channel, handler) {
+			console.log("presence init")
+			//emitter.on("presence", function(msg) {handler(msg.asObject())});
+			emitter.on("presence", function(msg) {console.log(msg)})
+			
+			emitter.presence({
+				key: emitterKey,
+				channel: baseChannel + "/" + channel,
+				status: true,
+				changes: true
+			})
+		}
 		
 	}		
 }
--- services/newGameInfo.js
+++ services/newGameInfo.js
@@ -1,6 +1,7 @@
 var newGameInfo = function ()
 {
     return	{
+		playerGUID: guidGenerator.getGuid(),
 		playerId: 0,
 		getOpponentId: function() { return (this.playerId + 1) % 2 },
 		playerNickname: "Player",
